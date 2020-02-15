561bc3cca97477f4a8e7a0ffff6aa6d4d510c3ea
faalbuquerque <bii.nett@gmail.com>
2020-02-15 20:46:56 -0300

    exemplo de troca de posicoes

--- fibonacci/script.js
+++ fibonacci/script.js
@@ -1,24 +1,48 @@
 
 let num1 = 0;
 
-
 let num2 = 1;
-
 
 let numR = num1+num2;
 
+let numR2 = numR + num2;
 
-let mumR2 = numR + num2;
+//console.log('valor numR: ', numR, 'valor numR2: ', numR2);
 
 
-
-
+for(i=0; i<1; i++){
+    temp = num2;
     num2 = numR;
-    numR = mumR2;
+    numR = numR2;
+
+    //console.log('valor num2: ', num2, 'valor numR: ', numR);
+}
 
 
+for(i=0; i<1; i++){
+    numR = num1+num2;
+    numR2 = numR + num2;
+    
+}
+
+index = prompt('fib em qual posicao')
+numAtual = 1
+numAnterior = 0
+resultadoParcial = ''
 
 
+for (let i = 1; i < index; i++) {
+     
+    resultadoParcial =  numAtual + numAnterior
+    numAnterior = numAtual
+    numAtual = resultadoParcial
+    
+    
+}
+
+console.log(resultadoParcial)
+
+/*
     let a = 5
     let b = 8
     let temp = "";
@@ -30,4 +54,4 @@ let mumR2 = numR + num2;
         temp = ''
     }
     console.log(`a: ${a} b: ${b} temp: ${temp}`)
-
+*/
