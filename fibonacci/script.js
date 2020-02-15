ed11d821c463be45552c38f8329e53f1d57a680a
faalbuquerque <bii.nett@gmail.com>
2020-02-15 19:33:42 -0300

    exemplo de troca de valores das variaveis

--- fibonacci/script.js
+++ fibonacci/script.js
@@ -1,13 +1,33 @@
 
 let num1 = 0;
 
+
 let num2 = 1;
+
 
 let numR = num1+num2;
 
-let mumR2 = numR + numR + num2;
+
+let mumR2 = numR + num2;
 
 
-num2 = numR;
 
-numR = mumR2 
+
+    num2 = numR;
+    numR = mumR2;
+
+
+
+
+    let a = 5
+    let b = 8
+    let temp = "";
+    console.log(`a: ${a} b: ${b} temp: ${temp}`)
+    for (let i = 0; i < 1; i++) {
+        temp = a
+        a = b
+        b = temp
+        temp = ''
+    }
+    console.log(`a: ${a} b: ${b} temp: ${temp}`)
+
